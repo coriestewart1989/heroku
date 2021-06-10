4a765184ed67cc1010e4a00284898aa1bc5ff10e
Corie Stewart <coriestewart1989>
2021-06-10 12:19:35 -0400

    added app and build

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 9000
+const path = require("path")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "client/build")))
+
+app.use("/api/", (_,res)=>{
+    res.json({data: "THIS IS THE API!!!!!"})
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
+})
+
+app.use("*", (_, res) =>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
+
+console.log("its alive!!!!!")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USERNAME)
+console.log(process.env.PORT)
+console.log(process.env.GREET)
